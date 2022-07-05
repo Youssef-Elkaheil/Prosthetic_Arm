7b3a7b0a0293434cceb68217dd3db09cdb4f093a
Youssef <61395452+Youssef-Elkaheil@users.noreply.github.com>
2022-07-05 17:47:43 +0200

    Init

--- Atmega32/02- MCAL/02- GIE/GIE_interface.h
+++
@@ -1,7 +0,0 @@
-#ifndef		GIE_INTERFACE_H
-#define     GIE_INTERFACE_H
-
-void MGIE_voidEnable(void);
-void MGIE_voidDisable(void);
-
-#endif
--- Atmega32/03- HAL/03-LM35/LM35_interface.h
+++
@@ -1,7 +0,0 @@
-#ifndef LM35_INTERFAVE_H
-#define LM35_INTERFAVE_H
-
-void HLM35_Init(void);
-double HLM35_Reading(u8 copy_Analog_PinID);
-
-#endif
